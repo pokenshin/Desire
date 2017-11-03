294fbc1e6bad663953bc7287d9617747be266f23
Jonas De Cunto <jonasdcau@gmail.com>
2017-11-03 00:28:06 -0200

    Desire.WinApp - Inserido campo de Pontos Gastos para controlar quantos pontos foram gastos nos atributos - Tamanho e posição das labels de atributo ajustadas. - Data binding com os campos que não são combo-box da identidade - Números que não são ValorMag muito grandes agora têm o tamanho de fonte diminuidos automaticamente. - Removidos boxes de magnitude em numeros que não tem magnitude

--- Desire.WinApp/MainWindow.xaml.cs
+++ Desire.WinApp/MainWindow.xaml.cs
@@ -38,18 +38,32 @@ namespace Desire.WinApp
             }
         }
 
+        private int pontosGastos;
+        public int PontosGastos
+        {
+            get { return ser.Forca.Pontos + ser.Materia.Pontos + ser.Destreza.Pontos + ser.Criatividade.Pontos + ser.Existencia.Pontos + ser.Ideia.Pontos + ser.Intelecto.Pontos; }
+            set
+            {
+                pontosGastos = value;
+                NotifyPropertyChanged("PontosGastos");
+            }
+        }
+
         private DbConsultas dbConsultas;
         private CalculadorSer calculadorSer;
 
+        private int tamanhoMaximoStringAtributos = 4;
+        private int tamanhoMaximoStringSubatributos = 5;
+
+
         public MainWindow()
         {
-            Personagem = new Ser();
             dbConsultas = new DbConsultas();
             calculadorSer = new CalculadorSer();
-            //geraSerAleatorio();
-            consultaAtributos();
             InitializeComponent();
             this.DataContext = this;
+            geraSerAleatorio();
+            atualizaSer();
         }
 
         private void consultaAtributos()
@@ -61,7 +75,7 @@ namespace Desire.WinApp
             ser.Criatividade = dbConsultas.RetornaCriatividade(ser.Criatividade.Pontos);
             ser.Existencia = dbConsultas.RetornaExistencia(ser.Existencia.Pontos);
             ser.Ideia = dbConsultas.RetornaIdeia(ser.Ideia.Pontos);
-            Personagem = ser;
+            atualizaSer();
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -79,9 +93,10 @@ namespace Desire.WinApp
             if (ser.Forca.Pontos < 110)
             {
                 ser.Forca.Pontos++;
+                NotifyPropertyChanged("PontosGastos");
                 ser.Forca = dbConsultas.RetornaForca(ser.Forca.Pontos);
                 ser = calculadorSer.CalculaSubatributos(ser);
-                Personagem = ser;
+                atualizaSer();
             }
         }
 
@@ -90,9 +105,10 @@ namespace Desire.WinApp
             if (ser.Forca.Pontos > 1)
             { 
                 ser.Forca.Pontos--;
+                NotifyPropertyChanged("PontosGastos");
                 ser.Forca = dbConsultas.RetornaForca(ser.Forca.Pontos);
                 ser = calculadorSer.CalculaSubatributos(ser);
-                Personagem = ser;
+                atualizaSer();
             }
         }
 
@@ -101,9 +117,10 @@ namespace Desire.WinApp
             if (ser.Materia.Pontos < 110)
             {
                 ser.Materia.Pontos++;
+                NotifyPropertyChanged("PontosGastos");
                 ser.Materia = dbConsultas.RetornaMateria(ser.Materia.Pontos);
                 ser = calculadorSer.CalculaSubatributos(ser);
-                Personagem = ser;
+                atualizaSer();
             }
         }
 
@@ -112,9 +129,10 @@ namespace Desire.WinApp
             if (ser.Materia.Pontos > 1)
             {
                 ser.Materia.Pontos--;
+                NotifyPropertyChanged("PontosGastos");
                 ser.Materia = dbConsultas.RetornaMateria(ser.Materia.Pontos);
                 ser = calculadorSer.CalculaSubatributos(ser);
-                Personagem = ser;
+                atualizaSer();
             }
         }
 
@@ -123,9 +141,10 @@ namespace Desire.WinApp
             if (ser.Destreza.Pontos < 110)
             {
                 ser.Destreza.Pontos++;
+                NotifyPropertyChanged("PontosGastos");
                 ser.Destreza = dbConsultas.RetornaDestreza(ser.Destreza.Pontos);
                 ser = calculadorSer.CalculaSubatributos(ser);
-                Personagem = ser;
+                atualizaSer();
             }
         }
 
@@ -134,9 +153,10 @@ namespace Desire.WinApp
             if (ser.Destreza.Pontos > 1)
             {
                 ser.Destreza.Pontos--;
+                NotifyPropertyChanged("PontosGastos");
                 ser.Destreza = dbConsultas.RetornaDestreza(ser.Destreza.Pontos);
                 ser = calculadorSer.CalculaSubatributos(ser);
-                Personagem = ser;
+                atualizaSer();
             }
         }
 
@@ -145,9 +165,10 @@ namespace Desire.WinApp
             if (ser.Intelecto.Pontos < 110)
             {
                 ser.Intelecto.Pontos++;
+                NotifyPropertyChanged("PontosGastos");
                 ser.Intelecto = dbConsultas.RetornaIntelecto(ser.Intelecto.Pontos);
                 ser = calculadorSer.CalculaSubatributos(ser);
-                Personagem = ser;
+                atualizaSer();
             }
         }
 
@@ -156,9 +177,10 @@ namespace Desire.WinApp
             if (ser.Intelecto.Pontos > 1)
             {
                 ser.Intelecto.Pontos--;
+                NotifyPropertyChanged("PontosGastos");
                 ser.Intelecto = dbConsultas.RetornaIntelecto(ser.Intelecto.Pontos);
                 ser = calculadorSer.CalculaSubatributos(ser);
-                Personagem = ser;
+                atualizaSer();
             }
         }
 
@@ -167,9 +189,10 @@ namespace Desire.WinApp
             if (ser.Criatividade.Pontos < 110)
             {
                 ser.Criatividade.Pontos++;
+                NotifyPropertyChanged("PontosGastos");
                 ser.Criatividade = dbConsultas.RetornaCriatividade(ser.Criatividade.Pontos);
                 ser = calculadorSer.CalculaSubatributos(ser);
-                Personagem = ser;
+                atualizaSer();
             }
         }
 
@@ -178,9 +201,10 @@ namespace Desire.WinApp
             if (ser.Criatividade.Pontos > 1)
             {
                 ser.Criatividade.Pontos--;
+                NotifyPropertyChanged("PontosGastos");
                 ser.Criatividade = dbConsultas.RetornaCriatividade(ser.Criatividade.Pontos);
                 ser = calculadorSer.CalculaSubatributos(ser);
-                Personagem = ser;
+                atualizaSer();
             }
         }
 
@@ -189,9 +213,10 @@ namespace Desire.WinApp
             if (ser.Existencia.Pontos < 110)
             {
                 ser.Existencia.Pontos++;
+                NotifyPropertyChanged("PontosGastos");
                 ser.Existencia = dbConsultas.RetornaExistencia(ser.Existencia.Pontos);
                 ser = calculadorSer.CalculaSubatributos(ser);
-                Personagem = ser;
+                atualizaSer();
             }
         }
 
@@ -200,9 +225,10 @@ namespace Desire.WinApp
             if (ser.Existencia.Pontos > 1)
             {
                 ser.Existencia.Pontos--;
+                NotifyPropertyChanged("PontosGastos");
                 ser.Existencia = dbConsultas.RetornaExistencia(ser.Existencia.Pontos);
                 ser = calculadorSer.CalculaSubatributos(ser);
-                Personagem = ser;
+                atualizaSer();
             }
         }
 
@@ -211,9 +237,10 @@ namespace Desire.WinApp
             if (ser.Ideia.Pontos < 110)
             {
                 ser.Ideia.Pontos++;
+                NotifyPropertyChanged("PontosGastos");
                 ser.Ideia = dbConsultas.RetornaIdeia(ser.Ideia.Pontos);
                 ser = calculadorSer.CalculaSubatributos(ser);
-                Personagem = ser;
+                atualizaSer();
             }
         }
 
@@ -222,15 +249,23 @@ namespace Desire.WinApp
             if (ser.Ideia.Pontos > 1)
             {
                 ser.Ideia.Pontos--;
+                NotifyPropertyChanged("PontosGastos");
                 ser.Ideia = dbConsultas.RetornaIdeia(ser.Ideia.Pontos);
                 ser = calculadorSer.CalculaSubatributos(ser);
-                Personagem = ser;
+                atualizaSer();
             }
         }
 
         private void cmb_aleatorio_Click(object sender, RoutedEventArgs e)
         {
             geraSerAleatorio();
+            atualizaSer();
+        }
+
+        private void atualizaSer()
+        {
+            validaTamanhoLabelsAtributos();
+            validaTamanhoLabelsSubatributos();
             Personagem = ser;
         }
 
@@ -251,6 +286,66 @@ namespace Desire.WinApp
             ser.Ideia = dbConsultas.RetornaIdeia(rng.GerarEntre(1, 110, rnd));
 
             ser = calculadorSer.CalculaSubatributos(ser);
+            lbl_rei_valor.Content = ser.Reis.Count();
+            NotifyPropertyChanged("PontosGastos");
+            atualizaSer();
+        }
+
+        private void validaTamanhoLabelsAtributos()
+        {
+            if (ser.Destreza.Dinamica.ToString().Count() > tamanhoMaximoStringAtributos)
+                lbl_destreza_dinamica_vl.FontSize = 24 - ((ser.Destreza.Dinamica.ToString().Count() - tamanhoMaximoStringAtributos) * 2);
+            else
+                lbl_destreza_dinamica_vl.FontSize = 24;
+
+            if (ser.Destreza.Iniciativa.ToString().Count() > tamanhoMaximoStringAtributos)
+                lbl_destreza_iniciativa_vl.FontSize = 24 - ((ser.Destreza.Iniciativa.ToString().Count() - tamanhoMaximoStringAtributos) * 2);
+            else
+                lbl_destreza_iniciativa_vl.FontSize = 24;
+
+            if (ser.Existencia.Plano.ToString().Count() > tamanhoMaximoStringAtributos)
+                lbl_existencia_plano_vl.FontSize = 24 - ((ser.Existencia.Plano.ToString().Count() - tamanhoMaximoStringAtributos) * 2);
+            else
+                lbl_existencia_plano_vl.FontSize = 24;
+
+            if (ser.Ideia.Ki.ToString().Count() > tamanhoMaximoStringAtributos)
+                lbl_ideia_ki_vl.FontSize = 24 - ((ser.Ideia.Ki.ToString().Count() - tamanhoMaximoStringAtributos) * 2);
+            else
+                lbl_ideia_ki_vl.FontSize = 24;
+
+            if (ser.Ideia.Base.ToString().Count() > tamanhoMaximoStringAtributos)
+                lbl_ideia_base_vl.FontSize = 24 - ((ser.Ideia.Base.ToString().Count() - tamanhoMaximoStringAtributos) * 2);
+            else
+                lbl_ideia_base_vl.FontSize = 24;
+        }
+
+        private void validaTamanhoLabelsSubatributos()
+        {
+            if (ser.BonusAP.ToString().Count() > tamanhoMaximoStringSubatributos)
+                lbl_apbonus_vl.FontSize = 22 - ((ser.BonusAP.ToString().Count() - tamanhoMaximoStringSubatributos) * 3);
+            else
+                lbl_apbonus_vl.FontSize = 22;
+
+            if (ser.BonusSP.ToString().Count() > tamanhoMaximoStringSubatributos)
+                lbl_spbonus_vl.FontSize = 22 - ((ser.BonusSP.ToString().Count() - tamanhoMaximoStringSubatributos) * 3);
+            else
+                lbl_spbonus_vl.FontSize = 22;
+
+            if (ser.Iniciativa.ToString().Count() > tamanhoMaximoStringSubatributos)
+                lbl_iniciativa_vl.FontSize = 22 - ((ser.Iniciativa.ToString().Count() - tamanhoMaximoStringSubatributos) * 3);
+            else
+                lbl_iniciativa_vl.FontSize = 22;
+
+            if (ser.Turno.ToString().Count() > tamanhoMaximoStringSubatributos)
+                lbl_turno_vl.FontSize = 22 - ((ser.Turno.ToString().Count() - tamanhoMaximoStringSubatributos) * 3);
+            else
+                lbl_turno_vl.FontSize = 22;
+
+            if (ser.Destria.ToString().Count() > tamanhoMaximoStringSubatributos)
+                lbl_destria_vl.FontSize = 22 - ((ser.Destria.ToString().Count() - tamanhoMaximoStringSubatributos) * 3);
+            else
+                lbl_destria_vl.FontSize = 22;
+
         }
     }
 }
