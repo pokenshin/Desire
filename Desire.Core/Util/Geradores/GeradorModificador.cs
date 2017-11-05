7c37aa5d74dc3d313dbf631135dca41db3778763
Jonas De Cunto <jonasdcau@gmail.com>
2017-11-05 17:04:33 -0200

    Desire.Core - Gerador de Modificadores alterado para refletir alterações na classe de Modificador.

--- Desire.Core/Util/Geradores/GeradorModificador.cs
+++ Desire.Core/Util/Geradores/GeradorModificador.cs
@@ -60,20 +60,101 @@ namespace Desire.Core.Util.Geradores
         {
             GeradorInteiro rng = new GeradorInteiro();
             GeradorValorMag rvmg = new GeradorValorMag();
-            int tipoModificador = rng.GerarEntre(1, 12, rnd);
+            GeradorPericia genPericia = new GeradorPericia();
+            GeradorHabilidade genHabilidade = new GeradorHabilidade();
+            int tipoModificador = rng.GerarEntre(1, 10, rnd);
             PropertyInfo[] propriedades = typeof(Ser).GetTypeInfo().DeclaredProperties.Where(p => p.PropertyType == typeof(int)).ToArray<PropertyInfo>();
             string alvo = propriedades[rng.GerarEntre(0, propriedades.Count() - 1, rnd)].Name;
 
             switch (tipoModificador)
             {
                 case 1:
-                    ModSomaValorMag modificador = new ModSomaValorMag()
+                    ModSomaValorMag modSomaValorMag = new ModSomaValorMag()
                     {
                         Alvo = alvo,
                         Origem = origem,
                         Valor = rvmg.GerarEntre(new ValorMag(10, 1), new ValorMag(99, 10), rnd)
                     };
-                    return modificador;
+                    return modSomaValorMag;
+
+                case 2:
+                    ModSubtraiValorMag modSubtraiValorMag = new ModSubtraiValorMag()
+                    {
+                        Alvo = alvo,
+                        Origem = origem,
+                        Valor = rvmg.GerarEntre(new ValorMag(10, 1), new ValorMag(99, 10), rnd)
+                    };
+                    return modSubtraiValorMag;
+
+                case 3:
+                    ModSomaNumero modSomaNumero = new ModSomaNumero()
+                    {
+                        Alvo = alvo,
+                        Origem = origem,
+                        Valor = rng.GerarEntre(1, 10000, rnd)
+                    };
+                    return modSomaNumero;
+
+                case 4:
+                    ModSubtraiNumero modSubtraiNumero = new ModSubtraiNumero()
+                    {
+                        Alvo = alvo,
+                        Origem = origem,
+                        Valor = rng.GerarEntre(1, 10000, rnd)
+                    };
+                    return modSubtraiNumero;
+
+                case 5:
+                    ModMultiplicaNumero modMultiplicaNumero = new ModMultiplicaNumero()
+                    {
+                        Alvo = alvo,
+                        Origem = origem,
+                        Valor = rng.GerarEntre(1, 10000, rnd)
+                    };
+                    return modMultiplicaNumero;
+
+                case 6:
+                    ModMultiplicaValorMag modMultiplicaValorMag = new ModMultiplicaValorMag()
+                    {
+                        Alvo = alvo,
+                        Origem = origem,
+                        Valor = rvmg.GerarEntre(new ValorMag(10, 1), new ValorMag(99, 10), rnd)
+                    };
+                    return modMultiplicaValorMag;
+
+                case 7:
+                    ModDivideNumero modDivideNumero = new ModDivideNumero()
+                    {
+                        Alvo = alvo,
+                        Origem = origem,
+                        Valor = rng.GerarEntre(1, 100, rnd)
+                    };
+                    return modDivideNumero;
+
+                case 8:
+                    ModDivideValorMag modDivideValorMag = new ModDivideValorMag()
+                    {
+                        Alvo = alvo,
+                        Origem = origem,
+                        Valor = rvmg.GerarEntre(new ValorMag(10, 1), new ValorMag(99, 5), rnd)
+                    };
+                    return modDivideValorMag;
+
+                case 9:
+                    ModAdicionaHabilidade modAdicionaHabilidade = new ModAdicionaHabilidade()
+                    {
+                        Alvo = genHabilidade.Gerar(rnd),
+                        Origem = origem
+                    };
+                    return modAdicionaHabilidade;
+
+                case 10:
+                    ModAdicionaPericia modAdicionaPericia = new ModAdicionaPericia()
+                    {
+                        Alvo = genPericia.Gerar(rnd),
+                        Origem = origem
+                    };
+                    return modAdicionaPericia;
 
                 default:
                     return null;
